dcd14dc7d918285d3c900790ae4745b992ce4395
rutvijashah <rutvijkumar.shah@gmail.com>
2014-07-03 16:49:22 -0700

    fixing java compilation issue in test code was refactored in text editor :)

--- tests/src/codepath/watsiapp/test/WatsiMainActivityTest.java
+++ tests/src/codepath/watsiapp/test/WatsiMainActivityTest.java
@@ -6,23 +6,30 @@ import codepath.watsiapp.R;
 import android.app.Activity;
 import android.widget.Button;
 import android.widget.TextView;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+
 
 public class WatsiMainActivityTest extends
     ActivityInstrumentationTestCase2<WatsiMainActivity> {
-  
-  Activity mainActivity = null;
-  Button button = null;
 
-  public WatsiMainActivityTest() {
-    super(MainActivity.class);
-  }
-  
-  @Override
-  protected void setUp() throws Exception {
-    super.setUp(); 
-  }
-  
-  public void testChangeText() {
-  }
+	 Activity mainActivity = null;
+	  Button button = null;
 
+	  public WatsiMainActivityTest() {
+	    super(WatsiMainActivity.class);
+	  }
+	  
+	  @Override
+	  protected void setUp() throws Exception {
+	    super.setUp();
+	    mainActivity = getActivity();
+	  }
+	  
+	  public void testChangeText() {
+	    ////button = (Button) mainActivity.findViewById(R.id.button_test);
+	    //TouchUtils.clickView(this, button);
+	    //extView view = (TextView) mainActivity.findViewById(R.id.textView1);
+	    assertEquals("t", "t");
+	  }
 }
