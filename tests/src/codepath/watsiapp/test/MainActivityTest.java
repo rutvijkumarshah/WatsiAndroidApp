952f9df11ee1cad2a05e9624c5134e9e3202f4b5
rutvijashah <rutvijkumar.shah@gmail.com>
2014-07-03 15:18:05 -0700

    Build setup for Android app

---
+++ tests/src/codepath/watsiapp/test/MainActivityTest.java
@@ -0,0 +1,28 @@
+package codepath.watsiapp.test;
+
+import codepath.watsiapp.WatsiMainActivity;
+import codepath.watsiapp.R;
+
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class WatsiMainActivityTest extends
+    ActivityInstrumentationTestCase2<WatsiMainActivity> {
+  
+  Activity mainActivity = null;
+  Button button = null;
+
+  public WatsiMainActivityTest() {
+    super(MainActivity.class);
+  }
+  
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+   
+  
+  public void testChangeText() {
+   
+
+}
