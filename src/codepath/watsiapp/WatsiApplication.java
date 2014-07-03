509aec058a8c8af1b49c8bffff888e95684c0544
rutvijashah <rutvijkumar.shah@gmail.com>
2014-07-03 10:49:36 -0700

    Init commit with project structure and Parse Data integration

---
+++ src/codepath/watsiapp/WatsiApplication.java
@@ -0,0 +1,36 @@
+package codepath.watsiapp;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+public class WatsiApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		// Add your initialization code here
+		Parse.initialize(this, "tezu5wFV9Z59i6HRSmKdfbcfv3wuUh84QlLrFOjJ", "SfhVI8iQxgdS63YMEPABsLOTaHoEoc78wJOQJFyS");
+
+
+		ParseUser.enableAutomaticUser();
+		ParseACL defaultACL = new ParseACL();
+	    
+		// If you would like all objects to be private by default, remove this line.
+		defaultACL.setPublicReadAccess(true);
+		
+		ParseACL.setDefaultACL(defaultACL, true);
+		testParseIntegration();
+	}
+	
+	private void testParseIntegration() {
+		ParseObject testObject = new ParseObject("TestObject");
+		testObject.put("foo", "bar");
+		testObject.saveInBackground();
+	}
+
+}
