c3800404cfece032cd0bc41d501ed242faa113a9
rutvijashah <rutvijkumar.shah@gmail.com>
2014-07-03 21:46:33 -0700

    Patient initial model object : Todo: image_file and medical_partner relationship

--- src/codepath/watsiapp/models/Patient.java
+++ src/codepath/watsiapp/models/Patient.java
@@ -22,6 +22,54 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 package codepath.watsiapp.models;
 
-public class Patient {
+import java.util.Date;
 
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+@ParseClassName("Patient")
+public class Patient extends ParseObject {
+
+	
+	public String getFirstName() {
+		return getString("firstName");
+		
+	}
+	public String getLastName() {
+		return getString("lastName");
+	}
+	public Date getDateAdded() {
+		return getDate("createdAt");
+	}
+	public String getMedicalNeed() {
+		return getString("medicalNeed");
+	}
+	public double getTargetDonation() {
+		return (Double) getNumber("targetDonation");
+		
+	}
+	public double getDonationReceived() {
+		return (Double)getNumber("donationReceived");
+	}
+	public boolean isFullyFunded() {
+		return (Boolean) getBoolean("isFullyFunded");
+	}
+	public Date getDateFullyFunded() {
+		return getDate("dateFullyFunded");
+	}
+	public int getAge() {
+		return (Integer)getNumber("age");
+	}
+	public String getCountry() {
+		return getString("country");
+	}
+	public String getStory() {
+		return getString("story");
+	}
+	
+	
+	//image , medical partner
+	
+	
 }
+
