34da19c8c4a554396cb757cefbe6c4db174bca34
Ankit Garg <gargankit90@gmail.com>
2014-07-12 17:21:25 -0700

    Making MedicalPartner work.

--- src/codepath/watsiapp/fragments/PatientDetailFragment.java
+++ src/codepath/watsiapp/fragments/PatientDetailFragment.java
@@ -1,6 +1,7 @@
 package codepath.watsiapp.fragments;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 
@@ -17,6 +18,7 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import codepath.watsiapp.R;
+import codepath.watsiapp.models.MedicalPartner;
 import codepath.watsiapp.models.Patient;
 import codepath.watsiapp.utils.Util;
 
@@ -33,23 +35,6 @@ public class PatientDetailFragment extends Fragment {
 	private ProgressBar donationProgress;
 	private TextView medicalPartner;
 	private TextView story;
-//
-//	// View lookup cache
-//	private static class ViewHolder {
-//		TextView name;
-//		TextView age;
-//		TextView location;
-//		TextView percentageFunded;
-//		TextView donationTogo;
-//		TextView medicalNeed;
-//		ImageView patientPhoto;
-//		ProgressBar donationProgress;
-//		Button donateBtn;
-//		ImageView shareAction;
-//		String patientId;
-//	}
-//
-//	private ViewHolder viewHolder;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -61,18 +46,16 @@ public class PatientDetailFragment extends Fragment {
 		try {
 			patientObj = patientObj.fetchIfNeeded();
 		} catch (ParseException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-//		viewHolder = new ViewHolder();
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Defines the xml file for the fragment
-		View view = inflater.inflate(R.layout.fragment_patient_detail,
-				container, false);
+		View view = inflater.inflate(R.layout.fragment_patient_detail,container, false);
+		// Assign views to local variables
 		name = (TextView) view.findViewById(R.id.tvName);
 		age = (TextView) view.findViewById(R.id.tvAge);
 		location = (TextView) view.findViewById(R.id.tvLocationVal);
@@ -81,9 +64,9 @@ public class PatientDetailFragment extends Fragment {
 		medicalNeed = (TextView) view.findViewById(R.id.tvPatientNeed);
 		patientPhoto = (ImageView) view.findViewById(R.id.ivPhoto);
 		donationProgress = (ProgressBar) view.findViewById(R.id.pbDonationProgress);
-		medicalPartner = (TextView) view.findViewById(R.id.tvMedicalPartner);
+		medicalPartner = (TextView) view.findViewById(R.id.tvMedicalPartnerVal);
 		story = (TextView) view.findViewById(R.id.tvDescription);
-		
+		// Set views to with values
 		name.setText(patientObj.getFullName());
 		age.setText(patientObj.getAge()+ " Years Old");
 		location.setText(patientObj.getCountry());
@@ -93,35 +76,15 @@ public class PatientDetailFragment extends Fragment {
 		medicalNeed.setText(patientObj.getMedicalNeed());
 		donationProgress.setProgress(donationProgressPecentage);
 		//TODO Make Medical Partner work.
-		//		medicalPartner.setText(patientObj.getMedicalPartner().getName());
+		patientObj.getMedicalPartner(new GetCallback<MedicalPartner>() {
+			@Override
+			public void done(MedicalPartner object, ParseException arg1) {
+				medicalPartner.setText(object.getName());
+			}
+		});
 		ImageLoader imageLoader = ImageLoader.getInstance();
 		imageLoader.displayImage(patientObj.getPhotoUrl(), patientPhoto);
 		story.setText(patientObj.getStory());
-		
-//		viewHolder.name = (TextView) view.findViewById(R.id.tvName);
-//		viewHolder.age = (TextView) view.findViewById(R.id.tvAge);
-//		viewHolder.location = (TextView) view.findViewById(R.id.tvLocationVal);
-//		 viewHolder.percentageFunded = (TextView) view
-//		 .findViewById(R.id.tvDonationStatus);
-//		
-//		 viewHolder.donationTogo = (TextView) view
-//		 .findViewById(R.id.tvDonationTogo);
-//
-//		viewHolder.medicalNeed = (TextView) view
-//				.findViewById(R.id.tvPatientNeed);
-//
-//		viewHolder.patientPhoto = (ImageView) view.findViewById(R.id.ivPhoto);
-//
-//		viewHolder.donationProgress = (ProgressBar) view
-//				.findViewById(R.id.pbDonationProgress);
-//		setupUI(patientObj);
-		//
-		// viewHolder.donateBtn = (Button) view
-		// .findViewById(R.id.donateBtn);
-		// viewHolder.shareAction = (ImageView) view
-		// .findViewById(R.id.shareIv);
-		// Setup handles to view objects here
-		// etFoo = (EditText) v.findViewById(R.id.etFoo);
 		return view;
 	}
 
@@ -132,49 +95,4 @@ public class PatientDetailFragment extends Fragment {
 		fragmentObj.setArguments(args);
 		return fragmentObj;
 	}
-
-//	private void setupUI(final Patient patient) {
-//
-//		// Add and download the image
-//		// patient photo
-////		ImageLoader imageLoader = ImageLoader.getInstance();
-////		imageLoader
-////				.displayImage(patient.getPhotoUrl(), viewHolder.patientPhoto);
-//
-//		// int donationProgressPecentage =
-//		// patient.getDonationProgressPecentage();
-//		// // donation progress
-//		// viewHolder.donationProgress.setProgress(donationProgressPecentage);
-//		//
-//		// // name
-//		// viewHolder.name.setText(patient.getFullName());
-//		//
-//		// // age
-//		// viewHolder.age.setText(patient.getAge() + " Years Old"); // TODO will
-//		// be
-//		// // externalized
-//		// // String as
-//		// // template
-//		// // string
-//		//
-//		// // location
-//		// viewHolder.location.setText(patient.getCountry());
-//		//
-//		// // percentageFunded
-//		// viewHolder.percentageFunded.setText(donationProgressPecentage
-//		// + " % Funded"); // TODO will be externalized String as template
-//		// // string
-//		//
-//		// // donationTOGO
-//		// viewHolder.donationTogo.setText("$ " + patient.getDonationToGo()
-//		// + " to go");// TODO will be externalized String as template
-//		// // string
-//		//
-//		// // medical need
-//		// viewHolder.medicalNeed.setText(patient.getMedicalNeed());
-//		//
-//		// viewHolder.patientId = patient.getObjectId();
-//
-//	}
-
 }
