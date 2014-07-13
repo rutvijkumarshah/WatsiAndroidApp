5179a612bf9f095ff84f80c34ab4f6f5e28acfc0
Rutvij <sharu03@SHARU03-W7.ca.com>
2014-07-12 17:35:37 -0700

    intermideate commit to preserve profile ui changes

--- src/codepath/watsiapp/activities/DonarProfileActivity.java
+++ src/codepath/watsiapp/activities/DonarProfileActivity.java
@@ -21,16 +21,14 @@
 
 package codepath.watsiapp.activities;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.TextView;
 import codepath.watsiapp.R;
 
+import com.facebook.Session;
+import com.facebook.android.Facebook;
+import com.parse.ParseFacebookUtils;
 import com.parse.ParseUser;
 
 
@@ -44,30 +42,14 @@ public class DonarProfileActivity extends Activity {
     super.onCreate(savedInstanceState);
 
     setContentView(R.layout.activity_donor_profile);
-    titleTextView = (TextView) findViewById(R.id.profile_title);
-    emailTextView = (TextView) findViewById(R.id.profile_email);
-    nameTextView = (TextView) findViewById(R.id.profile_name);
-    titleTextView.setText(R.string.profile_title_logged_in);
+//    titleTextView = (TextView) findViewById(R.id.profile_title);
+//    emailTextView = (TextView) findViewById(R.id.profile_email);
+//    nameTextView = (TextView) findViewById(R.id.profile_name);
+//    titleTextView.setText(R.string.profile_title_logged_in);
 
-    findViewById(R.id.logout_button).setOnClickListener(new OnClickListener() {
-      @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-      @Override
-      public void onClick(View v) {
-        ParseUser.logOut();
-
-        // FLAG_ACTIVITY_CLEAR_TASK only works on API 11, so if the user
-        // logs out on older devices, we'll just exit.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-          Intent intent = new Intent(DonarProfileActivity.this,
-              ParseDispatchActivity.class);
-          intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK
-              | Intent.FLAG_ACTIVITY_NEW_TASK);
-          startActivity(intent);
-        } else {
-          finish();
-        }
-      }
-    });
+    Session session = ParseFacebookUtils.getSession();
+    ParseUser u=null;
+    
   }
 
   @Override
@@ -84,12 +66,12 @@ public class DonarProfileActivity extends Activity {
    * @param user
    */
   private void showProfile(ParseUser user) {
-    if (user != null) {
-      emailTextView.setText(user.getEmail());
-      String fullName = user.getString("name");
-      if (fullName != null) {
-        nameTextView.setText(fullName);
-      }
-    }
+//    if (user != null) {
+//      emailTextView.setText(user.getEmail());
+//      String fullName = user.getString("name");
+//      if (fullName != null) {
+//        nameTextView.setText(fullName);
+//      }
+//    }
   }
 }
