360bd4d1786924156b366569cfdd8452219111fb
Rutvij <rutvijkumar.shah@gmail.com>
2014-07-18 18:30:53 -0700

    Fix #39

--- src/codepath/watsiapp/fragments/NewPatientDetailFragment.java
+++ src/codepath/watsiapp/fragments/NewPatientDetailFragment.java
@@ -1,6 +1,9 @@
 package codepath.watsiapp.fragments;
 
-
+import static codepath.watsiapp.utils.Util.startFundTreatmentIntent;
+import static codepath.watsiapp.utils.Util.startShareIntent;
+import static codepath.watsiapp.utils.Util.startShareIntentWithFaceBook;
+import static codepath.watsiapp.utils.Util.startShareIntentWithTwitter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -17,6 +20,7 @@ import codepath.watsiapp.R;
 import codepath.watsiapp.models.MedicalPartner;
 import codepath.watsiapp.models.Patient;
 import codepath.watsiapp.utils.Util;
+
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -24,21 +28,25 @@ import com.viewpagerindicator.CirclePageIndicator;
 import com.viewpagerindicator.IconPagerAdapter;
 
 public class NewPatientDetailFragment extends Fragment {
-	
+
 	private String patientId;
 	private Patient patientObj;
-	
-	private TextView name;
+
+	//private TextView name;
 	private TextView donationToGo;
 	private ProgressBar donationProgress;
 	private TextView medicalPartner;
 	private TextView story;
 	private ImageView fullyFundedCheckMark;
-	
+
 	private TestFragmentAdapter mAdapter;
 	private ViewPager mPager;
 	private CirclePageIndicator mIndicator;
 
+	private ImageView shareOnTwitter;
+	private ImageView shareOnFacebook;
+	private ImageView donateView;
+	private ImageView shareAction;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -57,39 +65,49 @@ public class NewPatientDetailFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		final boolean isFullyFunded = patientObj.isFullyFunded();
 		// Defines the xml file for the fragment
-		View view = inflater.inflate(R.layout.fragment_new_patient_detail,container, false);
-		
-		fullyFundedCheckMark=(ImageView)view.findViewById(R.id.isFullyFunded);
+		View view = inflater.inflate(R.layout.fragment_new_patient_detail,
+				container, false);
+
+		fullyFundedCheckMark = (ImageView) view
+				.findViewById(R.id.isFullyFunded);
 		// Assign views to local variables
-		mAdapter = new TestFragmentAdapter(getActivity().getSupportFragmentManager());
+		mAdapter = new TestFragmentAdapter(getActivity()
+				.getSupportFragmentManager());
 
 		mPager = (ViewPager) view.findViewById(R.id.pager);
 		mPager.setAdapter(mAdapter);
 
-		mIndicator = (CirclePageIndicator)  view.findViewById(R.id.indicator);
+		mIndicator = (CirclePageIndicator) view.findViewById(R.id.indicator);
 		mIndicator.setViewPager(mPager);
 
-		if(patientObj.isFullyFunded()) {
-			fullyFundedCheckMark.setVisibility(View.VISIBLE);
-		}
-		name = (TextView) view.findViewById(R.id.name);
+	
+		//name = (TextView) view.findViewById(R.id.name);
 		donationToGo = (TextView) view.findViewById(R.id.donationToGo);
-		donationProgress = (ProgressBar) view.findViewById(R.id.donationProgress);
+//		donationProgress = (ProgressBar) view
+//				.findViewById(R.id.donationProgress);
 		medicalPartner = (TextView) view.findViewById(R.id.medicalPartnerVal);
 		story = (TextView) view.findViewById(R.id.patientStory);
 		// Set views to with values
-		name.setText(patientObj.getFullName());
-		int donationProgressPecentage = patientObj.getDonationProgressPecentage();
-		donationToGo.setText("$ " + patientObj.getDonationToGo()+ " to go");
-		donationProgress.setProgress(donationProgressPecentage);
-		story.setText(patientObj.getStory().replace("#$#$", "<p>"));
+		getActivity().getActionBar().setTitle(patientObj.getFullName());
+		
+		int donationProgressPecentage = patientObj
+				.getDonationProgressPecentage();
+		//donationProgress.setProgress(donationProgressPecentage);
+		
+		shareAction = (ImageView) view.findViewById(R.id.shareIv);
+		shareOnFacebook =(ImageView)view.findViewById(R.id.share_fb);
+		donateView=(ImageView)view.findViewById(R.id.fund_treatment);
+		shareOnTwitter=(ImageView) view.findViewById(R.id.share_tw);
+		
+		story.setText(patientObj.getStory().replace("#$#$", ""));
 		patientObj.getMedicalPartner(new GetCallback<MedicalPartner>() {
 			@Override
 			public void done(final MedicalPartner object, ParseException arg1) {
 				medicalPartner.setText(object.getName());
 				medicalPartner.setOnClickListener(new OnClickListener() {
-					
+
 					@Override
 					public void onClick(View v) {
 						Util.starShowMedicalPartnerIntent(getActivity(), object);
@@ -99,6 +117,53 @@ public class NewPatientDetailFragment extends Fragment {
 			}
 		});
 
+		shareAction.setTag(patientObj);
+		shareAction.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				startShareIntent(getActivity(),(Patient)v.getTag());
+
+			}
+		});
+		shareOnTwitter.setTag(patientObj);
+		shareOnTwitter.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				startShareIntentWithTwitter(getActivity(),(Patient)v.getTag());
+
+			}
+		});
+
+		shareOnFacebook.setTag(patientObj);
+		shareOnFacebook.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				startShareIntentWithFaceBook(getActivity(),(Patient)v.getTag());
+
+			}
+		});
+
+		if (isFullyFunded) {
+			fullyFundedCheckMark.setVisibility(View.VISIBLE);
+			donateView.setVisibility(View.INVISIBLE);
+		}else {
+			donationToGo
+					.setText("$ " + patientObj.getDonationToGo() + " to go");
+			donateView.setVisibility(View.VISIBLE);
+			
+			donateView.setTag(patientObj);
+			donateView.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					startFundTreatmentIntent(getActivity(),(Patient)v.getTag());
+				}
+			});
+			
+		}
 		return view;
 	}
 
@@ -109,8 +174,9 @@ public class NewPatientDetailFragment extends Fragment {
 		fragmentObj.setArguments(args);
 		return fragmentObj;
 	}
-	
-	class TestFragmentAdapter extends FragmentPagerAdapter  implements IconPagerAdapter{
+
+	class TestFragmentAdapter extends FragmentPagerAdapter implements
+			IconPagerAdapter {
 
 		public TestFragmentAdapter(FragmentManager fm) {
 			super(fm);
@@ -119,19 +185,23 @@ public class NewPatientDetailFragment extends Fragment {
 		@Override
 		public Fragment getItem(int position) {
 			if (position == 0) {
-				return PatientImageViewFragment.newInstance(patientObj.getPhotoUrl());
+				return PatientImageViewFragment.newInstance(patientObj
+						.getPhotoUrl());
 			} else {
-				String ageStr=null;
-				int age=patientObj.getAge();
-				if(age < 0) {
-					ageStr="baby";
+				String ageStr = null;
+				int age = patientObj.getAge();
+				if (age < 0) {
+					ageStr = "baby";
 				}
-				if(age == 1) {
-					ageStr=age+" year old";
-				}else {
-					ageStr=age+" years old";
+				if (age == 1) {
+					ageStr = age + " year old";
+				} else {
+					ageStr = age + " years old";
 				}
-				return PatientSummaryFragment.newInstance(patientObj.getMedicalNeed(), ageStr, patientObj.getCountry(), patientObj.getDonationProgressPecentage()+"% funded");
+				return PatientSummaryFragment.newInstance(
+						patientObj.getMedicalNeed(), ageStr,
+						patientObj.getCountry(),
+						patientObj.getDonationProgressPecentage() + "% funded");
 			}
 		}
 
@@ -150,7 +220,6 @@ public class NewPatientDetailFragment extends Fragment {
 			// TODO Auto-generated method stub
 			return 0;
 		}
-
 
 	}
 }
