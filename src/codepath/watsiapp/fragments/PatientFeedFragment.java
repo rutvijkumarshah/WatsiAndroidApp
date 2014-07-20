c186782a373c9fde1bc0fa5a38ff150eb764ecfc
Ganesh Sangle <sayganesha@gmail.com>
2014-07-19 17:19:47 -0700

    Hide action bar when scrolling down.

--- src/codepath/watsiapp/fragments/PatientFeedFragment.java
+++ src/codepath/watsiapp/fragments/PatientFeedFragment.java
@@ -8,6 +8,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
 import android.widget.ProgressBar;
 import codepath.watsiapp.R;
 import codepath.watsiapp.adapters.HomeFeedAdapter;
@@ -24,7 +26,7 @@ public class PatientFeedFragment extends Fragment {
 	private HomeFeedAdapter patientFeedAdapter;
 	private eu.erikw.PullToRefreshListView listView;
 	private ProgressBar progressBar;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,7 +38,7 @@ public class PatientFeedFragment extends Fragment {
 
 		View v = inflater.inflate(R.layout.fragment_patient_feed, container,
 				false);
-		
+
 		progressBar=(ProgressBar)v.findViewById(R.id.pf_progressBar);
 		patientFeedAdapter = new HomeFeedAdapter(getActivity());
 		patientFeedAdapter.addOnQueryLoadListener(new OnQueryLoadListener<NewsItem>() {
@@ -67,9 +69,36 @@ public class PatientFeedFragment extends Fragment {
 				progressBar.setVisibility(View.VISIBLE);
 			}
 		});
-	 
+
 		listView = (PullToRefreshListView) v.findViewById(R.id.patient_feed_list);
 		listView.setAdapter(patientFeedAdapter);
+		listView.setOnScrollListener(new OnScrollListener() 
+		{
+			int mLastFirstVisibleItem = 0;
+
+			@Override
+			public void onScrollStateChanged(AbsListView view, int scrollState) {   }           
+
+			@Override
+			public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) 
+			{   
+				if (view.getId() == listView.getId()) 
+				{
+					final int currentFirstVisibleItem = listView.getFirstVisiblePosition();
+
+					if (currentFirstVisibleItem > mLastFirstVisibleItem) 
+					{
+						getActivity().getActionBar().hide();
+					} 
+					else if (currentFirstVisibleItem < mLastFirstVisibleItem) 
+					{
+						getActivity().getActionBar().show();
+					}
+
+					mLastFirstVisibleItem = currentFirstVisibleItem;
+				}               
+			}
+		});
 		setupIintialViews();
 		return v;
 	}
