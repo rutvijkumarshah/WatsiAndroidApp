3581c9cdba4dd3755d81da70094d45a00192053a
Rutvij <rutvijkumar.shah@gmail.com>
2014-07-22 17:44:22 -0700

    Rotating watsi logo on splash screen

--- src/codepath/watsiapp/SplashScreenActivity.java
+++ src/codepath/watsiapp/SplashScreenActivity.java
@@ -5,6 +5,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreenActivity extends Activity {
 
@@ -21,7 +24,10 @@ public class SplashScreenActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
- 
+        ImageView logo=(ImageView)findViewById(R.id.logo);
+        Animation rotate = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        logo.startAnimation(rotate);
+
         new Handler().postDelayed(new Runnable() {
  
             /*
